608bdc4adbff98e1464bb163152215d7888374f8
Connor Peet <connor@connorpeet.com>
2014-05-25 08:23:35 -0500

    Handle malformed JSON/requests

--- src/Mcprohosting/Skinner/Skinner.php
+++ src/Mcprohosting/Skinner/Skinner.php
@@ -30,31 +30,37 @@ class Skinner {
 		return self::user($username);
 	}
 
-    /** 
-    * Function to pull the properly capitalised username and the UUID, will return {"id":"null","name":"steve"} if the username is invalid.
-    * @param string $username
-    * @return Json Object with UUID and properly capitalised Username
-    */
+	/** 
+	* Function to pull the properly capitalised username and the UUID, will return {"id":"null","name":"steve"} if the username is invalid.
+	* @param string $username
+	* @return Json Object with UUID and properly capitalised Username
+	*/
 	public static function getUserData($username) {
 
-        	$data_string = '["'.$username.'"]';                                                                                   
-	         
-	        $ch = curl_init('https://api.mojang.com/profiles/minecraft');                                                                      
-	        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-	        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-	        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);                                                                      
-	        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-	            'Content-Type: application/json',                                                                                
-	            'Content-Length: ' . strlen($data_string))                                                                       
-	        );                                                                                                                   
-	        $result = curl_exec($ch);
-	        if($result == "[]") {
-		    $userObject = array("id" => "null", "name" => "steve");
-	        }else{
-		    $jsonObject = json_decode($result, true);
-			$userObject = $jsonObject[0];
-	        }
-	        return $userObject;
+			$data_string = '["'.$username.'"]';                                                                                   
+			 
+			$ch = curl_init('https://api.mojang.com/profiles/minecraft');                                                                      
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);                                                                      
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+				'Content-Type: application/json',                                                                                
+				'Content-Length: ' . strlen($data_string))                                                                       
+			);                                                                                                                   
+			$result = curl_exec($ch);
+			if ($result == "[]") {
+				$userObject = array("id" => "null", "name" => "steve");
+			} else {
+				$jsonObject = json_decode($result, true);
+				
+				if (json_last_error() !== JSON_ERROR_NONE) {
+					return array("id" => "null", "name" => "steve");
+				}
+
+				$userObject = $jsonObject[0];
+			}
+
+			return $userObject;
 	}
 }
